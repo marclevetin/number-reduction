b5eac35d84db65dc16f76e5520f030f4a5ac2aa0
marclevetin <marc@derbyhat.org>
2018-02-20 22:02:50 -0500

    implemented BigNumber library

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const userEnteredNumber = parseFloat(process.argv[2]);
+// const userEnteredNumber = parseFloat(process.argv[2]);
+const userEnteredNumber = process.argv[2] || 'exit';
+const showWork = process.argv[3];
 const math = require('./mathFunctions');
+const BigNumber = require('big-number');
 
 // algorithm
 // if number is even, divide by two
@@ -16,26 +19,36 @@ const confirmed = {};
 // disabled validation for a moment
 let numberOfSteps = reduceNumber(userEnteredNumber, 0);
 console.log('number of steps: ' + numberOfSteps)
-console.log(confirmed)
+
+if (showWork) {
+  console.log('here are the calculations to get to this number')
+  console.log(confirmed)
+}
 
 function reduceNumber(number, iterator) {
   debugger;
-  // end state
-  if (number === 1) {
+  // exit if the user forgets to add a value.
+  // this prevents an error at the first use of .val()
+  if (number === 'exit') {
+    console.log('please enter a number')
+    return
+    //end state
+  } else if (BigNumber(number).val() === '1') {
+    // if (number === 1) {
     confirmed[userEnteredNumber] = iterator
     return iterator;
-
   // even
-  } else if (number % 2 === 0) {
+  } else if (BigNumber(number).mod(2).val() === '0') {
+  // } else if (number % 2 === 0) {
     iterator += 1;
     let newNumber = math.divideByTwo(number);
     return reduceNumber(newNumber, iterator);
 
   // odd
-  } else if (number % 2 === 1) {
+  } else if (BigNumber(number).mod(2).val() === '1') {
+  // } else if (number % 2 === 1) {
     // this section finds the most beneficial "next step" by running the entire sequence
     // even though it runs all the way to 1, it only returns a single number
-    // needs to be optimized
 
     let addNumber = math.addOne(number);
     if (!confirmed[addNumber]) {
@@ -50,7 +63,6 @@ function reduceNumber(number, iterator) {
     }
 
     // use the next step with fewer steps to 1, add 1 to iterator (for the next step)
-    // if (addSteps > subtractSteps) {
     if (confirmed[addNumber] > confirmed[subtractNumber]) {
       return reduceNumber(subtractNumber, iterator + 1)
     } else {
@@ -58,6 +70,7 @@ function reduceNumber(number, iterator) {
     }
 
   // something went wrong
+  // can't throw an error, lest Mocha have a heart attack
   } else {
     console.log('something went wrong')
   }
