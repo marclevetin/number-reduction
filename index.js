cd40821ab9181151e0915c1c0dc0083d2187a23c
marclevetin <marc@derbyhat.org>
2018-02-20 22:04:35 -0500

    clean up from BigNumber implementation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const userEnteredNumber = parseFloat(process.argv[2]);
 const userEnteredNumber = process.argv[2] || 'exit';
 const showWork = process.argv[3];
 const math = require('./mathFunctions');
@@ -16,57 +15,55 @@ const BigNumber = require('big-number');
 // return number of steps needed
 
 const confirmed = {};
-// disabled validation for a moment
+
 let numberOfSteps = reduceNumber(userEnteredNumber, 0);
-console.log('number of steps: ' + numberOfSteps)
+console.log('number of steps: ' + numberOfSteps);
 
 if (showWork) {
-  console.log('here are the calculations to get to this number')
-  console.log(confirmed)
+  console.log('here are the calculations to get to this number');
+  console.log(confirmed);
 }
 
 function reduceNumber(number, iterator) {
-  debugger;
   // exit if the user forgets to add a value.
   // this prevents an error at the first use of .val()
   if (number === 'exit') {
     console.log('please enter a number')
-    return
+    return;
+
     //end state
   } else if (BigNumber(number).val() === '1') {
-    // if (number === 1) {
     confirmed[userEnteredNumber] = iterator
     return iterator;
+
   // even
   } else if (BigNumber(number).mod(2).val() === '0') {
-  // } else if (number % 2 === 0) {
     iterator += 1;
     let newNumber = math.divideByTwo(number);
     return reduceNumber(newNumber, iterator);
 
   // odd
   } else if (BigNumber(number).mod(2).val() === '1') {
-  // } else if (number % 2 === 1) {
     // this section finds the most beneficial "next step" by running the entire sequence
     // even though it runs all the way to 1, it only returns a single number
 
     let addNumber = math.addOne(number);
     if (!confirmed[addNumber]) {
       let addSteps = reduceNumber(addNumber, 1);
-      confirmed[addNumber] = addSteps - 1
+      confirmed[addNumber] = addSteps - 1;
     }
 
     let subtractNumber = math.subtractOne(number);
     if (!confirmed[subtractNumber]) {
       let subtractSteps = reduceNumber(subtractNumber, 1);
-      confirmed[subtractNumber] = subtractSteps - 1
+      confirmed[subtractNumber] = subtractSteps - 1;
     }
 
     // use the next step with fewer steps to 1, add 1 to iterator (for the next step)
     if (confirmed[addNumber] > confirmed[subtractNumber]) {
-      return reduceNumber(subtractNumber, iterator + 1)
+      return reduceNumber(subtractNumber, iterator + 1);
     } else {
-      return reduceNumber(addNumber, iterator + 1)
+      return reduceNumber(addNumber, iterator + 1);
     }
 
   // something went wrong
--- test/mathFunctions.js
+++ test/mathFunctions.js
@@ -16,21 +16,18 @@ describe("Math", function() {
 
   it("divideByTwo returns the correct result", function() {
     expect(typeof mathFunctions.divideByTwo(4)).to.equal("string");
-    // expect(typeof mathFunctions.divideByTwo(4)).to.equal("number");
     expect(mathFunctions.divideByTwo(4)).to.equal("2");
     expect(mathFunctions.divideByTwo(-4)).to.equal("-2");
   });
 
   it("addOne returns the correct result", function() {
     expect(typeof mathFunctions.addOne(4)).to.equal("string");
-    // expect(typeof mathFunctions.addOne(4)).to.equal("number");
     expect(mathFunctions.addOne(4)).to.equal("5");
     expect(mathFunctions.addOne(-4)).to.equal("-3");
   });
 
   it("subtractOne returns the correct result", function() {
     expect(typeof mathFunctions.addOne(4)).to.equal("string");
-    // expect(typeof mathFunctions.addOne(4)).to.equal("number");
     expect(mathFunctions.subtractOne(4)).to.equal("3");
     expect(mathFunctions.subtractOne(-4)).to.equal("-5");
   });
